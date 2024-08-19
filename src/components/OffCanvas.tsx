4c0bda88e6f0b494f7a725db8d1b6e7edc188c5e
Vidhura Neethika <vidhuraneethika000@gmail.com>
2024-08-19 19:35:15 +0530

    Change offcanvas component to get additional classes

--- src/components/OffCanvas.tsx
+++ src/components/OffCanvas.tsx
@@ -1,10 +1,15 @@
 'use client';
 import {BsX} from "react-icons/bs";
 
-const OffCanvas = ({children, isOpen, setIsOpen}: { children: any, isOpen: boolean, setIsOpen: any }) => {
+const OffCanvas = ({children, isOpen, setIsOpen, className}: {
+    children: any,
+    isOpen: boolean,
+    setIsOpen: any,
+    className: string
+}) => {
     return (
         <div
-            className={`w-11/12 md:w-5/12 h-screen shadow-2xl shadow-neutral-200 rounded-r-xl fixed top-0 left-0 ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300`}>
+            className={`w-11/12 md:w-5/12 h-screen shadow-2xl shadow-neutral-200 rounded-r-xl fixed top-0 left-0 ${className} ${isOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300`}>
             <div className="w-full h-full relative">
                 <button className="p-1 rounded bg-black text-white absolute top-5 right-5 z-20"
                         onClick={() => setIsOpen(false)}>
